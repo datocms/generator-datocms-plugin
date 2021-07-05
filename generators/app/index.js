fcb442ad9c7a1f2dce7b96d7c31b244e07d5aa87
Stefano Verna <s.verna@datocms.com>
2021-07-05 09:55:55 +0200

    Update to latest yeoman

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,123 +1,123 @@
-const Generator = require("yeoman-generator");
-const changeCase = require("change-case");
-const { AccountClient, SiteClient } = require("datocms-client");
+const Generator = require('yeoman-generator');
+const changeCase = require('change-case');
+const { AccountClient, SiteClient } = require('datocms-client');
 
-const oauthToken = require("../../utils/oauthToken");
+const oauthToken = require('../../utils/oauthToken');
 
-const required = value => {
+const required = (value) => {
   if (value) {
     return true;
   }
 
-  return "Please insert some value!";
+  return 'Please insert some value!';
 };
 
-const atLeastOne = list => {
+const atLeastOne = (list) => {
   if (list && list.length > 0) {
     return true;
   }
 
-  return "Please insert some value!";
+  return 'Please insert some value!';
 };
 
 module.exports = class extends Generator {
   async prompting() {
     this.log();
-    this.log("Hi! I am your DatoCMS plugin generator!");
+    this.log('Hi! I am your DatoCMS plugin generator!');
     this.log(
-      "I will generate for you the structure of a Webpack project to start developing your shiny new DatoCMS plugin right away! Let's start, shall we? :)"
+      "I will generate for you the structure of a Webpack project to start developing your shiny new DatoCMS plugin right away! Let's start, shall we? :)",
     );
 
     const { packageName } = await this.prompt([
       {
-        type: "input",
-        name: "packageName",
+        type: 'input',
+        name: 'packageName',
         message:
-          "Please insert the NPM package name for this new plugin (it must start with datocms-plugin-)",
+          'Please insert the NPM package name for this new plugin (it must start with datocms-plugin-)',
         default: `datocms-plugin-${changeCase.paramCase(this.appname)}`,
-        validate: function(value) {
-          if (value.startsWith("datocms-plugin-")) {
+        validate: function (value) {
+          if (value.startsWith('datocms-plugin-')) {
             return true;
           } else {
-            return "You need to add a datocms-plugin- prefix!";
+            return 'You need to add a datocms-plugin- prefix!';
           }
-        }
-      }
+        },
+      },
     ]);
 
     this.answers = await this.prompt([
       {
-        type: "input",
-        name: "pluginTitle",
-        message: "Please insert a human name for this plugin",
+        type: 'input',
+        name: 'pluginTitle',
+        message: 'Please insert a human name for this plugin',
         default: changeCase.titleCase(
-          packageName.replace("datocms-plugin-", "")
+          packageName.replace('datocms-plugin-', ''),
         ),
-        validate: required
+        validate: required,
       },
       {
-        type: "input",
-        name: "description",
-        message: "Please insert a small description",
-        validate: required
+        type: 'input',
+        name: 'description',
+        message: 'Please insert a small description',
+        validate: required,
       },
       {
-        type: "list",
-        name: "pluginType",
+        type: 'list',
+        name: 'pluginType',
         message:
-          "What kind of plugin is it? (For more info see https://www.datocms.com/docs/plugins/introduction/#types-of-plugins)",
+          'What kind of plugin is it? (For more info see https://www.datocms.com/docs/plugins/introduction/#types-of-plugins)',
         choices: [
-          { name: "Field editor", value: "field_editor" },
-          { name: "Field add-on", value: "field_addon" },
-          { name: "Sidebar widget", value: "sidebar" }
+          { name: 'Field editor', value: 'field_editor' },
+          { name: 'Field add-on', value: 'field_addon' },
+          { name: 'Sidebar widget', value: 'sidebar' },
         ],
-        validate: required
+        validate: required,
       },
       {
-        type: "checkbox",
-        name: "fieldTypes",
-        message: "Which kind of fields is this plugin compatible with?",
+        type: 'checkbox',
+        name: 'fieldTypes',
+        message: 'Which kind of fields is this plugin compatible with?',
         choices: [
-          { name: "JSON", value: "json" },
-          { name: "Text", value: "text" },
-          { name: "Boolean", value: "boolean" },
-          { name: "Float", value: "float" },
-          { name: "Integer", value: "integer" },
-          { name: "String", value: "string" },
-          { name: "Multiple links", value: "links" },
-          { name: "Single link", value: "link" },
-          { name: "Date", value: "date" },
-          { name: "Date-time", value: "date_time" },
-          { name: "Video", value: "video" },
-          { name: "Color", value: "color" },
-          { name: "Seo", value: "seo" },
-          { name: "Geolocation", value: "lat_lon" }
+          { name: 'JSON', value: 'json' },
+          { name: 'Text', value: 'text' },
+          { name: 'Boolean', value: 'boolean' },
+          { name: 'Float', value: 'float' },
+          { name: 'Integer', value: 'integer' },
+          { name: 'String', value: 'string' },
+          { name: 'Multiple links', value: 'links' },
+          { name: 'Single link', value: 'link' },
+          { name: 'Date', value: 'date' },
+          { name: 'Date-time', value: 'date_time' },
+          { name: 'Video', value: 'video' },
+          { name: 'Color', value: 'color' },
+          { name: 'Seo', value: 'seo' },
+          { name: 'Geolocation', value: 'lat_lon' },
         ],
-        validate: atLeastOne
+        validate: atLeastOne,
       },
       {
-        type: "editor",
-        name: "parameterDefinitions",
+        type: 'editor',
+        name: 'parameterDefinitions',
         message:
-          "Please insert any configuration parameters this plugin requires (see https://www.datocms.com/docs/plugins/creating-a-new-plugin/#configuration-parameters)",
+          'Please insert any configuration parameters this plugin requires (see https://www.datocms.com/docs/plugins/creating-a-new-plugin/#configuration-parameters)',
         default: JSON.stringify(
           {
             global: [
               {
-                id: "developmentMode",
-                label: "Development mode",
-                type: "boolean",
+                id: 'developmentMode',
+                label: 'Development mode',
+                type: 'boolean',
                 required: true,
                 default: false,
-                hint: "Shows debug messages in console"
-              }
+                hint: 'Shows debug messages in console',
+              },
             ],
-            instance: []
+            instance: [],
           },
           null,
-          2
+          2,
         ),
-        validate: value => {
+        validate: (value) => {
           try {
             const defs = JSON.parse(value);
             if (
@@ -128,12 +128,12 @@ module.exports = class extends Generator {
             ) {
               return true;
             }
-            return "Please insert a valid JSON object";
+            return 'Please insert a valid JSON object';
           } catch (e) {
-            return "Not valid JSON";
+            return 'Not valid JSON';
           }
-        }
-      }
+        },
+      },
     ]);
 
     this.answers = Object.assign(
@@ -141,66 +141,66 @@ module.exports = class extends Generator {
       this.answers,
       await this.prompt([
         {
-          type: "input",
-          name: "keywords",
+          type: 'input',
+          name: 'keywords',
           message:
-            "Please add some tags to make the plugin more discoverable (comma separated)",
+            'Please add some tags to make the plugin more discoverable (comma separated)',
           validate: required,
           default: [changeCase.paramCase(this.answers.pluginType)]
             .concat(
               this.answers.fieldTypes.map(
-                ft => `${changeCase.paramCase(ft)}-field`
-              )
+                (ft) => `${changeCase.paramCase(ft)}-field`,
+              ),
             )
-            .join(", ")
+            .join(', '),
         },
         {
-          type: "input",
-          name: "homepage",
-          message: "Please add the homepage for this plugin",
+          type: 'input',
+          name: 'homepage',
+          message: 'Please add the homepage for this plugin',
           validate: required,
-          default: `https://github.com/YOUR-USER/${packageName}`
+          default: `https://github.com/YOUR-USER/${packageName}`,
         },
         {
-          type: "input",
-          name: "authorName",
+          type: 'input',
+          name: 'authorName',
           message: "What's your name?",
-          validate: required
+          validate: required,
         },
         {
-          type: "input",
-          name: "authorEmail",
+          type: 'input',
+          name: 'authorEmail',
           message: "What's your email?",
-          validate: required
+          validate: required,
         },
         {
-          type: "list",
-          name: "template",
-          message: "Please select the preferred template",
+          type: 'list',
+          name: 'template',
+          message: 'Please select the preferred template',
           choices: [
-            { name: "Vanilla JS", value: "vanilla" },
-            { name: "React", value: "react" }
+            { name: 'Vanilla JS', value: 'vanilla' },
+            { name: 'React', value: 'react' },
           ],
-          validate: required
+          validate: required,
         },
         {
-          type: "confirm",
-          name: "addToProject",
+          type: 'confirm',
+          name: 'addToProject',
           message:
-            "Would you like to add this plugin in development mode to one of your projects and start developing it right away?"
-        }
-      ])
+            'Would you like to add this plugin in development mode to one of your projects and start developing it right away?',
+        },
+      ]),
     );
 
     if (this.answers.addToProject) {
       await this.prompt([
         {
-          type: "confirm",
-          name: "foo",
+          type: 'confirm',
+          name: 'foo',
           message:
             "In order to add this plugin to one of your projects we need you to sign in to DatoCMS. We'll open up a browser whenever you're ready!",
-          validate: required
-        }
+          validate: required,
+        },
       ]);
 
       const credentials = await oauthToken();
@@ -210,15 +210,15 @@ module.exports = class extends Generator {
 
       const answers2 = await this.prompt([
         {
-          type: "list",
-          name: "site",
+          type: 'list',
+          name: 'site',
           message:
-            "On which project you would like to add this plugin in development mode?",
+            'On which project you would like to add this plugin in development mode?',
           choices: sites
-            .filter(site => site.owner === account.id)
-            .map(site => ({ name: site.name, value: site })),
-          validate: required
-        }
+            .filter((site) => site.owner === account.id)
+            .map((site) => ({ name: site.name, value: site })),
+          validate: required,
+        },
       ]);
 
       this.answers.site = answers2.site;
@@ -237,26 +237,26 @@ module.exports = class extends Generator {
       keywords,
       template,
       fieldTypes,
-      homepage
+      homepage,
     } = this.answers;
 
     this.fs.copy(
       this.templatePath(`${template}/**/*`),
       this.destinationRoot(),
-      { globOptions: { dot: true } }
+      { globOptions: { dot: true } },
     );
 
     this.fs.copyTpl(
       this.templatePath(`webpack.config.js.${template}`),
-      this.destinationPath("webpack.config.js"),
+      this.destinationPath('webpack.config.js'),
       {
-        packageName
-      }
+        packageName,
+      },
     );
 
     this.fs.copyTpl(
-      this.templatePath("package.json"),
-      this.destinationPath("package.json"),
+      this.templatePath('package.json'),
+      this.destinationPath('package.json'),
       {
         packageName,
         pluginTitle,
@@ -268,26 +268,26 @@ module.exports = class extends Generator {
         fieldTypes,
         homepage,
         parameterDefinitions: JSON.parse(parameterDefinitions),
-        keywords: ["datocms", "datocms-plugin"]
+        keywords: ['datocms', 'datocms-plugin']
           .concat(
             [changeCase.paramCase(this.answers.pluginType)],
             this.answers.fieldTypes.map(
-              ft => `${changeCase.paramCase(ft)}-field`
+              (ft) => `${changeCase.paramCase(ft)}-field`,
             ),
-            keywords.split(/\s*,\s*/)
+            keywords.split(/\s*,\s*/),
           )
-          .filter((value, index, self) => self.indexOf(value) === index)
-      }
+          .filter((value, index, self) => self.indexOf(value) === index),
+      },
     );
 
     this.fs.copyTpl(
-      this.templatePath("README.md"),
-      this.destinationPath("README.md"),
+      this.templatePath('README.md'),
+      this.destinationPath('README.md'),
       {
         pluginTitle,
         description,
-        packageName
-      }
+        packageName,
+      },
     );
 
     if (this.answers.site) {
@@ -298,52 +298,50 @@ module.exports = class extends Generator {
         fieldTypes,
         pluginType,
         parameterDefinitions: JSON.parse(parameterDefinitions),
-        url: `http://localhost:5000/`
+        url: `http://localhost:5000/`,
       });
     }
-  }
-
-  install() {
-    this.installDependencies({
-      npm: false,
-      bower: false,
-      yarn: true
-    });
   }
 
   end() {
     this.log();
     this.log(
-      `Hurray! The plugin structure is ready! You can start the development server with the following command:`
+      `Hurray! The plugin structure is ready! You can start the development server with the following command:`,
     );
 
     this.log();
-    this.log(`yarn start`);
+    this.log(`npm run start`);
     this.log();
 
     if (this.plugin) {
       this.log(
-        `The plugin has already been installed in development mode on the "${this.answers.site.name}" project: https://${this.answers.site.internalSubdomain}.admin.datocms.com/admin/plugins/${this.plugin.id}/edit`
+        `The plugin has already been installed in development mode on the "${this.answers.site.name}" project: https://${this.answers.site.internalSubdomain}.admin.datocms.com/admin/plugins/${this.plugin.id}/edit`,
       );
       this.log(`Just open src/index.js and start developing right away!`);
     } else {
       this.log(
-        `You can add this plugin to one of your DatoCMS projects following this guide: https://www.datocms.com/docs/plugins/creating-a-new-plugin/`
+        `You can add this plugin to one of your DatoCMS projects running:`,
+      );
+      this.log();
+      this.log(`npm run addToProject`);
+      this.log();
+      this.log(
+        `Or following this guide: https://www.datocms.com/docs/plugins/creating-a-new-plugin/`,
       );
     }
 
     this.log();
     this.log(
-      `For more help on how to build plugins, take a look at our documentation! https://www.datocms.com/docs/plugins/entry-point/`
+      `For more help on how to build plugins, take a look at our documentation! https://www.datocms.com/docs/plugins/entry-point/`,
     );
     this.log();
 
     this.log(
-      `Once you've finished developing this plugin, remember to publish it with the following command:`
+      `Once you've finished developing this plugin, remember to publish it with the following command:`,
     );
 
     this.log();
-    this.log(`yarn publish`);
+    this.log(`npm publish`);
     this.log();
   }
 };
